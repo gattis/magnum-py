cd19492656deedeb13107106adee216dca61c746
gattis <gattis@bdabd2ca-bbc1-11de-82ee-51df2961d3cf>
2009-10-22 03:39:20 +0000

    reverted bad checkin to config file

--- config.py
+++ config.py
@@ -3,20 +3,20 @@
 
 #### Example config file for serving django and static files ####
 
-import django.core.handlers.wsgi
-import magnum.http
-import magnum.http.wsgi
-import magnum.http.static
-import magnum.http.dispatch
+# import django.core.handlers.wsgi
+# import magnum.http
+# import magnum.http.wsgi
+# import magnum.http.static
+# import magnum.http.dispatch
 
-WORKERS = 10
-HOST = ('mattgattis.com', 8080)
-HANDLER_CLASS = magnum.http.dispatch.DispatchWrapper({
-        "^/media/": magnum.http.static.StaticWrapper("/var/www/matt/media/","/media/"),
-        "^/about/": magnum.http.static.StaticWrapper("/var/www/matt/about/","/about/"),
-        "^/favicon.ico$": magnum.http.static.StaticWrapper("/var/www/matt/media/","/"),
-        "default": magnum.http.wsgi.WSGIWrapper(django.core.handlers.wsgi.WSGIHandler())
-      })
-DEBUG = True
-PID_FILE = '/tmp/magnum-dev.pid'
-LOG_FILE = '/var/log/magnum-dev.out'
+# WORKERS = 10
+# HOST = ('localhost', 80)
+# HANDLER_CLASS = magnum.http.dispatch.DispatchWrapper({
+#        "^/media/": magnum.http.static.StaticWrapper("/home/www/media/","/media/"),
+#        "^/about/": magnum.http.static.StaticWrapper("/home/www/about_html/","/about/"),
+#        "^/favicon.ico$": magnum.http.static.StaticWrapper("/home/www/media/","/"),
+#        "default": magnum.http.wsgi.WSGIWrapper(django.core.handlers.wsgi.WSGIHandler())
+#      })
+# DEBUG = True
+# PID_FILE = '/tmp/magnum.pid'
+# LOG_FILE = '/var/log/magnum.out'
